50c5d2f85549e5e54f1def6cfdedaed02cb53651
prassu56 <prasuprasanna236@gmail.com>
2024-12-05 14:01:07 +0530

    Add files via upload

---
+++ Forcountingdivisors1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Forcountingdivisors1{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int j=0;j<t;j++)
+        {
+            int n=sc.nextInt();
+            int c=0;
+            for(int i=1;i<=n;i++)
+            {
+                if(n%i==0){
+                c+=1;
+            }
+        }
+
+         System.out.println(c);
+            }
+            
+        }
+}
