018c8f9d090fbb368462a62c26a6c4cf0d465dd6
prassu56 <prasuprasanna236@gmail.com>
2024-12-05 12:34:30 +0530

    Add files via upload

---
+++ Forodd1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Forodd1{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+        }
