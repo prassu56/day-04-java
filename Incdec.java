455210eba1a9565aa2ed062a42bdc6f8fe0c0c66
prassu56 <prasuprasanna236@gmail.com>
2024-12-05 16:12:28 +0530

    Add files via upload

---
+++ Incdec.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Incdec{
+    public static void main(String[]args){
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+        int b=6;
+        System.out.println(--b);
+        int a1=56;
+        System.out.println(a1--);
+         System.out.println(a1--);
+
+    }
+}
