88a3d67024d8ab763dcde624199011459c93d2d0
prassu56 <prasuprasanna236@gmail.com>
2024-12-05 12:42:19 +0530

    Add files via upload

---
+++ Fordivisors.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Fordivisors{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            for(int j=1;j<n;j++)
+            {
+                if(n%j==0)
+
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
