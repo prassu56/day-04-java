037e8ae1338f1b78e387ff68987b1cb6cf7e95c6
prassu56 <prasuprasanna236@gmail.com>
2024-12-05 12:28:53 +0530

    Add files via upload

---
+++ Formultipleof8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Formultipleof8{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<=t;i++)
+        {
+            int n=sc.nextInt();
+            if((n&7)==0)
+        {
+            System.out.println("multiple of 8");
+            }else{
+              System.out.println(" not multiple of 8");
+            }
+        }
+    }}
