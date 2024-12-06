5a00c8ae1c4893745322f06517736f8afc0e7931
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 11:06:38 +0530

    Add files via upload

---
+++ Printingnnaturalnumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Printingnnaturalnumbers{
+    public static void main(String[]args){
+        int n=10;
+        int i=n-1;
+        while(i>0)
+        {
+            System.out.println(n-i);
+            i-=1;
+        }
+
+    }
+    
+}
