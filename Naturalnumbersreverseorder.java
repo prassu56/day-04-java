28b4faed17913f948d57b0aaffea5be05216c218
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 11:11:35 +0530

    Add files via upload

---
+++ Naturalnumbersreverseorder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Naturalnumbersreverseorder{
+    public static void main(String[]args){
+        int n=100;
+        int i=n;
+        while(i>-1)
+        {
+            System.out.println(i);
+            i-=1;
+        }
+
+    }
+    
+}
